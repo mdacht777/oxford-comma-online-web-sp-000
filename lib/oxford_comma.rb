9198fe08afe62fe2c94d04b2d801998e92ea9d5e
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 15:03:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,14 @@
 def oxford_comma(array)
   i=0
+  str=""
   puts (i+1)<array.count
   array.each do
     array.index
     if i>0
-      array[i]+=", "
+      str+=", "
+      str+=array[i]
     end
     i+=1
   end
-  str=array.join
   str
 end
