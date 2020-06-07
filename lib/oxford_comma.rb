34f2af8eb2caa5deec17617081b8f23af0db6e45
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 15:00:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,8 @@ def oxford_comma(array)
   i=0
   puts (i+1)<array.count
   array.each do
-    if (i+1)==array.count && array.count>1
-    array[i]+=" and "
-    elsif (i+1)<array.count
-    array[i]+=", "
-    end
+    if i>0
+      array[i]+=", "
     i+=1
   end
   str=array.join
