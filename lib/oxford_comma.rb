6285e56246220275f14d26a50d19ec0b9df0dc69
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 15:05:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   puts (i+1)<array.count
   array.each do
     array.index
-    if (i+1)==array.count
+    if (i+1)==array.count && array.count>1
       str+=" and "
     elsif i>0
       str+=", "
