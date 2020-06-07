61abf15e7125674ba556f530c5c7122833dd9536
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 14:59:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   i=0
   puts (i+1)<array.count
   array.each do
-    if (i+1)<array.count
-    array[i]+=", "
-    elsif (i+1)==array.count && array.count>1
+    if (i+1)==array.count && array.count>1
     array[i]+=" and "
+    elsif (i+1)<array.count
+    array[i]+=", "
     end
     i+=1
   end
